99bfc5892fbeaf173f35b74f4e0b473927d2f0a6
Paulius Jacikevicius <p.jacikevicius@gmail.com>
2022-11-21 16:42:35 +0200

    Divided input_and_detaisls_cart to widgets.

--- lib/view/widgets/recipe_cards/input_and_details_card.dart
+++ lib/view/widgets/recipe_cards/input_and_details_card.dart
@@ -54,121 +54,144 @@ class InputAndDetailsCard extends StatelessWidget {
             builder: (context, constraint) {
               return Padding(
                 padding: const EdgeInsets.only(top: 8),
-                child: Column(
-                  children: isAdd
-                      ? [
-                          BuildTextField(
-                            flex: 1,
-                            controller: _mealNameController,
-                            fieldLabel: "Enter Meal Name",
+                child: isAdd
+                    ? _addRecipeForm(
+                        constraint: constraint,
+                        mealNameController: _mealNameController,
+                        descriptionController: _descriptionController,
+                      )
+                    : isDetails
+                        ? _recipeDetailsCard(
+                            recipe: recipe,
                             constraint: constraint,
-                            textStyle: Theme.of(context)
-                                .textTheme
-                                .headlineMedium!
-                                .copyWith(
-                                  color: Colors.black,
-                                ),
+                          )
+                        : Center(
+                            child: Text('Something went wrong!'),
                           ),
-                          BuildTextField(
-                            flex: 4,
-                            controller: _mealNameController,
-                            fieldLabel: "Meal Description",
-                            constraint: constraint,
-                            textStyle: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                  color: Colors.black,
-                                ),
-                          ),
-                          BuildCategoryCarousel(),
-                          BuildTmePicker(),
-                          Expanded(
-                            child: GestureDetector(
-                              child: Expanded(
-                                flex: 1,
-                                child: BuildButton(
-                                  buttonText: 'Upload Recipe',
-                                  buttonColor: Theme.of(context).primaryColor,
-                                  textStyle: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium!
-                                      .copyWith(
-                                        color: Colors.black,
-                                      ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ]
-                      : isDetails
-                          ? [
-                              BuildText(
-                                flex: 1,
-                                constraint: constraint,
-                                fieldLabel: recipe!.name,
-                                textStyle: Theme.of(context)
-                                    .textTheme
-                                    .headlineMedium!
-                                    .copyWith(
-                                      color: Colors.black,
-                                    ),
-                              ),
-                              BuildText(
-                                flex: 5,
-                                constraint: constraint,
-                                fieldLabel: recipe!.recipeDesc,
-                                textStyle: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(
-                                      color: Colors.black,
-                                    ),
-                              ),
-                              BuildDurationText(
-                                flex: 1,
-                                constraint: constraint,
-                                cookTime: recipe!.cookTime,
-                                textStyle: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(
-                                      color: Colors.black,
-                                    ),
-                              ),
-                              Expanded(
-                                child: GestureDetector(
-                                  behavior: HitTestBehavior.opaque,
-                                  onTap: () {
-                                    CustomPopUpCard(context, true);
-                                  },
-                                  child: Expanded(
-                                    flex: 1,
-                                    child: BuildButton(
-                                      buttonText: 'Komentuoti',
-                                      buttonColor:
-                                          Theme.of(context).primaryColor,
-                                      textStyle: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium!
-                                          .copyWith(
-                                            color: Colors.black,
-                                          ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ]
-                          : [
-                              Center(
-                                child: Text('Something went wrong!'),
-                              ),
-                            ],
-                ),
               );
             },
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _recipeDetailsCard extends StatelessWidget {
+  const _recipeDetailsCard({
+    Key? key,
+    required this.recipe,
+    required this.constraint,
+  }) : super(key: key);
+
+  final Recipe? recipe;
+  final BoxConstraints constraint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        BuildText(
+          flex: 1,
+          constraint: constraint,
+          fieldLabel: recipe!.name,
+          textStyle: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                color: Colors.black,
+              ),
+        ),
+        BuildText(
+          flex: 5,
+          constraint: constraint,
+          fieldLabel: recipe!.recipeDesc,
+          textStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Colors.black,
+              ),
+        ),
+        BuildDurationText(
+          flex: 1,
+          constraint: constraint,
+          cookTime: recipe!.cookTime,
+          textStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Colors.black,
+              ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              CustomPopUpCard(context, true);
+            },
+            child: Expanded(
+              flex: 1,
+              child: BuildButton(
+                buttonText: 'Komentuoti',
+                buttonColor: Theme.of(context).primaryColor,
+                textStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.black,
+                    ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _addRecipeForm extends StatelessWidget {
+  const _addRecipeForm({
+    Key? key,
+    required TextEditingController mealNameController,
+    required TextEditingController descriptionController,
+    required this.constraint,
+  })  : _mealNameController = mealNameController,
+        _descriptionController = descriptionController,
+        super(key: key);
+
+  final TextEditingController _mealNameController;
+  final TextEditingController _descriptionController;
+  final BoxConstraints constraint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          BuildTextField(
+            flex: 1,
+            controller: _mealNameController,
+            fieldLabel: "Enter Meal Name",
+            constraint: constraint,
+            textStyle: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                  color: Colors.black,
+                ),
+          ),
+          BuildTextField(
+            flex: 4,
+            controller: _descriptionController,
+            fieldLabel: "Meal Description",
+            constraint: constraint,
+            textStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.black,
+                ),
+          ),
+          BuildCategoryCarousel(),
+          BuildTmePicker(),
+          Expanded(
+            child: GestureDetector(
+              child: Expanded(
+                flex: 1,
+                child: BuildButton(
+                  buttonText: 'Upload Recipe',
+                  buttonColor: Theme.of(context).primaryColor,
+                  textStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Colors.black,
+                      ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
